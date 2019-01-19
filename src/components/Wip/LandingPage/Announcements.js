e0dcb502bcdaea4abba44828cc1d0e9bfedfc771
Jesus Palos <jesus.rpalos@gmail.com>
2019-01-19 17:56:45 -0600

    Added comments tellin ya wassup

--- src/components/Wip/LandingPage/Announcements.js
+++ src/components/Wip/LandingPage/Announcements.js
@@ -4,6 +4,9 @@ import {Container} from "reactstrap";
 /**
  * Component for announcements, not limited to events, but including things like signups to stuff
  * dates of volunteer events, and news immediately relevant to our members.
+ * TODO:
+ * Announcements will be retrieved via an API Call to our backend,
+ * So we'll need at least some mock data to be able to complete this
  */
 export default class Announcements extends React.Component {
   render() {
