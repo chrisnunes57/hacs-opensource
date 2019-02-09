725621c161b872de77170e6098f2e01ada59e4c2
Jesus Palos <jesus.rpalos@gmail.com>
2019-02-09 15:46:40 -0600

    Adding contact links

---
+++ src/components/Wip/LandingPage/ContactLinks.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Container} from "reactstrap";
+
+
+
+export default class ContactLinks extends React.Component {
+
+    render(){
+        return(
+            <Container>
+
+            </Container>
+        )
+    }
+}
