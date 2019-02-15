7a0c6b0438107f85fbb198aff69637f182e9a1e7
AJ Marquez <ajmarquez@AJs-MacBook-Pro-2.local>
2019-02-15 16:46:54 -0600

    Added corporate logos.

--- src/data/sponsorList.js
+++ src/data/sponsorList.js
@@ -1,6 +1,8 @@
-import Sponsor1 from "../images/logo.svg";
-import Sponsor2 from "../images/logo.svg";
+import Sponsor1 from "../images/PaloAlto.png";
+import Sponsor2 from "../images/Tableau.png";
 import Sponsor3 from "../images/logo.svg";
+import Sponsor4 from "../images/logo.svg";
+import SponsorIcon from "../components/Wip/Corporate/SponsorIcon";
 
 /**
  * This is dummy data for now, but will need the name of all our sponsors (maybe)
@@ -11,16 +13,20 @@ import Sponsor3 from "../images/logo.svg";
 export const SponsorList = {
   sponsors: [
     {
-      name: "dummy name 1",
+      name: "Palo Alto",
       icon: Sponsor1
     },
     {
-      name: "dummy name 2",
+      name: "Tableau",
       icon: Sponsor2
     },
     {
       name: "dummy name 3",
       icon: Sponsor3
+    },
+    {
+      name: "dummy name 4",
+      icon: Sponsor4
     }
   ]
 };
