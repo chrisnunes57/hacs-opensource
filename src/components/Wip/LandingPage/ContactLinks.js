4f4cbafee079da632c8e5e4c914e5ff534914d0c
Manuel <maniponce22@gmail.com>
2019-02-15 12:21:21 -0600

    adding protected email

--- src/components/Wip/LandingPage/ContactLinks.js
+++ src/components/Wip/LandingPage/ContactLinks.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import {Container} from "reactstrap";
+import {Container, Row,Col} from "reactstrap";
 import TwitterLogo from './logoPics/twitter.png'
 import FacebookLogo from './logoPics/facebook.png'
 import InstagramLogo from './logoPics/instagram.png'
+import EmailLogo from './logoPics/email.png'
+import Obfuscate from 'react-obfuscate'
 
 
 export default class ContactLinks extends React.Component {
@@ -10,16 +12,37 @@ export default class ContactLinks extends React.Component {
     render(){
         return(
             <Container>
-                <h1>HACS Contact Links</h1>
-                <a  target="_blank" href={"https://twitter.com/texashacs?lang=en"}>
-                    <img src={TwitterLogo} alt="Twitter Logo" style={{width:'100px'}} />
-                </a>
-                <a target="_blank" href={"https://www.instagram.com/texashacs/"}>
-                    <img src={InstagramLogo} alt="Instagram Logo" style={{width:'125px', padding: '15px'}} />
-                </a>
-                <a target="_blank" href={"https://www.facebook.com/groups/TexasHACS"}>
-                    <img src={FacebookLogo} alt="Facebook Logo" style={{width:'95px'}} />
-                </a>
+                <div
+                style={{
+                backgroundColor: "#F4B24C",
+                width: "100%",
+                borderRadius: 20,
+                overflow: "hidden", 
+                margin: "5px"}}>
+                <h1 style={{textAlign: "center"}}>
+                    {" "}
+                    Contact Links{" "}
+                 </h1>
+                </div>
+                <h1 style={{textAlign: "center"}}>
+                    <a  target="_blank" href={"https://twitter.com/texashacs?lang=en"}>
+                        <img src={TwitterLogo} alt="Twitter Logo" style={{width:'100px'}} />
+                    </a>
+                    <a target="_blank" href={"https://www.instagram.com/texashacs/"}>
+                        <img src={InstagramLogo} alt="Instagram Logo" style={{width:'150px', padding: '25px'}} />
+                    </a>
+                    <a target="_blank" href={"https://www.facebook.com/groups/TexasHACS"}>
+                        <img src={FacebookLogo} alt="Facebook Logo" style={{width:'95px'}} />
+                    </a>
+                    <Row>
+                        <Col>
+                        <img src={EmailLogo} alt="Email Logo" style={{width:'75px'}} />
+                        <Obfuscate element={'span'} >
+                        Texashacs@gmail.com
+                        </Obfuscate>
+                        </Col>
+                    </Row>
+                </h1>
             </Container>
         )
     }
