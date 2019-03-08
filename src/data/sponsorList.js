cdcb2f37dbe37a1816dd68a947d87e55db2953fb
AJ Marquez <ajmarquez@wireless-10-145-219-163.public.utexas.edu>
2019-03-08 14:33:26 -0600

    Finished adding sponsor images.

--- src/data/sponsorList.js
+++ src/data/sponsorList.js
@@ -1,7 +1,15 @@
-import Sponsor1 from "../images/PaloAlto.png";
-import Sponsor2 from "../images/Tableau.png";
-import Sponsor3 from "../images/logo.svg";
-import Sponsor4 from "../images/logo.svg";
+import Sponsor1 from "../images/dropbox.png";
+import Sponsor2 from "../images/facebook.png";
+import Sponsor3 from "../images/godaddy.png";
+import Sponsor4 from "../images/indeed.png";
+import Sponsor5 from "../images/lyft.png";
+import Sponsor6 from "../images/microsoft.png";
+import Sponsor7 from "../images/PaloAlto.png";
+import Sponsor8 from "../images/quip.png";
+import Sponsor9 from "../images/quorared.png";
+import Sponsor10 from "../images/statestreet.png";
+import Sponsor11 from "../images/Tableau.png";
+import Sponsor12 from "../images/target.png";
 import SponsorIcon from "../components/Wip/Corporate/SponsorIcon";
 
 /**
@@ -13,20 +21,52 @@ import SponsorIcon from "../components/Wip/Corporate/SponsorIcon";
 export const SponsorList = {
   sponsors: [
     {
-      name: "Palo Alto",
+      name: "Dropbox",
       icon: Sponsor1
     },
     {
-      name: "Tableau",
+      name: "Facebook",
       icon: Sponsor2
     },
     {
-      name: "dummy name 3",
+      name: "GoDaddy",
       icon: Sponsor3
     },
     {
-      name: "dummy name 4",
+      name: "Indeed",
       icon: Sponsor4
+    },
+    {
+      name: "Lyft",
+      icon: Sponsor5
+    },
+    {
+      name: "Microsoft",
+      icon: Sponsor6
+    },
+    {
+      name: "Palo Alto",
+      icon: Sponsor7
+    },
+    {
+      name: "Quip",
+      icon: Sponsor8
+    },
+    {
+      name: "Quora",
+      icon: Sponsor9
+    },
+    {
+      name: "State Street",
+      icon: Sponsor10
+    },
+    {
+      name: "Tableau",
+      icon: Sponsor11
+    },
+    {
+      name: "Target",
+      icon: Sponsor12
     }
   ]
 };
