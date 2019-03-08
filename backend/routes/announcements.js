0066f59336e9bbdf959fe6e9a04ab84735b89495
Jesus Palos <jesus.rpalos@gmail.com>
2019-03-08 17:56:51 -0600

    enable cors

--- backend/routes/announcements.js
+++ backend/routes/announcements.js
@@ -3,6 +3,13 @@ const router = express.Router();
 const { Announcements} = require('../database');
 
 // Routes
+
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 router.get('/', (request, response, next) => {
   Announcements.getAll().then( (result) => {
     response.status(200).json(result);
