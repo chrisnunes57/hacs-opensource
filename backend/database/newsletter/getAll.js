feb247ff9664c8b0c74fe06f7ac7ad9287f0e3df
Manuel <maniponce22@gmail.com>
2019-03-15 11:50:40 -0500

    Added get all for newletters and officers

--- backend/database/newsletter/getAll.js
+++ backend/database/newsletter/getAll.js
@@ -2,15 +2,15 @@ const Sequelize = require('sequelize');
 const db = require('../connection');
 
 // Sequel Query
-const RENAME_THIS_QUERY = ``;
+const GET_ALL_NEWSLETTERS = `SELECT * FROM newsletters`;
 
 const getAll = () => {
-  return new Promise((resolve, reject) => {
-    resolve({
-      msg: "TODO"
-    });
+    return db.query(GET_ALL_NEWSLETTERS, {
+    type: Sequelize.QueryTypes.SELECT
   });
 };
+
+
 
 // Export this call to the rest of the application
 module.exports = getAll;
--- backend/database/officers/getAll.js
+++ backend/database/officers/getAll.js
@@ -2,13 +2,11 @@ const Sequelize = require('sequelize');
 const db = require('../connection');
 
 // Sequel Query
-const RENAME_THIS_QUERY = ``;
+const GET_ALL_OFFICERS = `SELECT * FROM officers`;
 
 const getAll = () => {
-  return new Promise((resolve, reject) => {
-    resolve({
-      msg: "TODO"
-    });
+    return db.query(GET_ALL_OFFICERS, {
+     type: Sequelize.QueryTypes.SELECT
   });
 };
 
