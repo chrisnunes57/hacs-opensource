38b3e14911a4bb60e03c61a617ecb73d723ca14c
Manuel <maniponce22@gmail.com>
2019-03-29 14:24:28 -0500

    changed seed date to pass tests

--- backend/database/newsletter/getAll.js
+++ backend/database/newsletter/getAll.js
@@ -6,11 +6,9 @@ const GET_ALL_NEWSLETTERS = `SELECT * FROM newsletters`;
 
 const getAll = () => {
     return db.query(GET_ALL_NEWSLETTERS, {
-    type: Sequelize.QueryTypes.SELECT
+     type: Sequelize.QueryTypes.SELECT
   });
 };
-
-
 
 // Export this call to the rest of the application
 module.exports = getAll;
