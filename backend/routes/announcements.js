fd1cadfea3b53de60b1d974daded6e04d00c8e10
Robert <rquinones93@yahoo.com>
2019-04-05 14:30:04 -0700

    Announcements removeById route
    Updated Announcements tests to include create and remove integration test

--- backend/routes/announcements.js
+++ backend/routes/announcements.js
@@ -25,11 +25,24 @@ router.post('/create', (request, response, next) => {
   const { title, content } = request.body;
 
   Announcements.create( title, content ).then( () => {
-    response.status(201).json( { msg: `Announcement "${title}" has been saved.`});
+    response.status(201).json( { msg: `Announcement "${title}" has been saved.`} );
   
   }).catch( (error) => {
     response.status(400).json( { error: "New announcement was not created." } );
   
+  });
+});
+
+router.post('/remove/:id', (request, response, next) => {
+  // TODO - Validate and Sanitize Inputs
+
+  Announcements.removeById( request.params.id ).then( () => {
+    response.status(200).json( { msg: `Removed Announcement ID: ${request.params.id}` } );
+
+  }).catch( (error) => {
+    console.log(error);
+    response.status(400).json( { error: `Unable to remove Announcement ID: ${request.params.id}` } );
+
   });
 });
 
