2d12f692a51e6ae334aca8d3310cc2c14cbe1af7
Jesus Palos <jesus.rpalos@gmail.com>
2019-04-12 05:08:27 -0500

    added routes for officers, mentorship, and sponsors

---
+++ backend/routes/sponsors.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const { Sponsors } = require("../database");
+
+// Routes
+
+router.use((request, response, next) => {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+router.get("/", (request, response, next) => {
+  Sponsors.getAll()
+    .then(result => {
+      response.status(200).json(result);
+    })
+    .catch(error => {
+      response.status(400).json(error);
+    });
+});
+
+router.get("/:type", (request, response, next) => {
+  Sponsors.getByPackageType(request.params.type)
+    .then(result => {
+      response.status(200).json(result);
+    })
+    .catch(error => {
+      response.status(400).json(error);
+    });
+});
+
+module.exports = router;
