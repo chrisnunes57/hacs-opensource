d852c448e9150aaa7da173b75d7350044ced1c1b
Manuel <maniponce22@gmail.com>
2019-05-01 22:20:21 -0500

    messing with front end for sponsorship

--- src/components/Pages/Corporate/CorporatePage.js
+++ src/components/Pages/Corporate/CorporatePage.js
@@ -2,15 +2,19 @@ import React from "react";
 import SponsorPackage from "./SponsorPackage";
 import Sponsors from "./Sponsors";
 import Grid from "@material-ui/core/Grid/index";
+import HowToSponsor from "./HowToSponsor";
 
 export default class CorporatePage extends React.Component {
   render() {
     return (
-      <Grid container direction="row" justify="center" spacing={8}>
+      <Grid container direction="row" spacing={8} >
+        <Grid item xs={6}>
+          <HowToSponsor style/>
+        </Grid>
         <Grid item xs={12}>
           <SponsorPackage />
         </Grid>
-        <Grid item xs={12}>
+        <Grid item xs={12} >
           <Sponsors />
         </Grid>
       </Grid>
