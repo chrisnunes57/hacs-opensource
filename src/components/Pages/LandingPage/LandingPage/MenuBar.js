3219501eb50360b705eb8627b67faed928088efc
Manuel <maniponce22@gmail.com>
2019-05-07 18:00:56 -0500

    Updating NavBar

---
+++ src/components/Pages/LandingPage/LandingPage/MenuBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default class MenuBar extends React.Component {
+    render() {
+      return (
+          <Grid item xs={{12}}>
+          </Grid>
+      );
+    }
+}
