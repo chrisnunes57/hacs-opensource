bb6665f72d4d3434edf9438c3be042512f1f450c
Chris Nunes <chrisnunes57@gmail.com>
2020-09-10 13:51:32 -0500

    social icon now have links

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,11 +6,21 @@ function Header() {
     <div className="global-header">
       <h3>Hispanic Association of Computer Scientists</h3>
       <div className="socials">
-        <i class="fab fa-facebook-f" />
-        <i class="fab fa-twitter" />
-        <i class="fab fa-instagram" />
-        <i class="far fa-envelope" />
-        <i class="fab fa-github" />
+        <a href="https://www.facebook.com/groups/TexasHACS" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-facebook-f" />
+        </a>
+        <a href="https://twitter.com/texasHACS" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-twitter" />
+        </a>
+        <a href="https://www.instagram.com/texashacs/?hl=en" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-instagram" />
+        </a>
+        <a href="mailto:texashacs@gmail.com" target="_blank" rel="noopener noreferrer">
+            <i class="far fa-envelope" />
+        </a>
+        <a href="https://github.com/chrisnunes57/hacs-opensource" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-github" />
+        </a>
       </div>
     </div>
   );
