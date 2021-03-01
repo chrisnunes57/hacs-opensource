c73c91c5b8ad7c62b494f3bf86336b5163bd0ed5
Jeffrey Moulckers <jrmoulckers@gmail.com>
2021-03-01 00:24:02 -0600

    added styling for scholarships with placeholder data and reveal

--- src/components/Opportunities/index.js
+++ src/components/Opportunities/index.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState, useEffect } from "react";
+import Fade from "react-reveal/Fade";
 import "./Opportunities.scss";
 import {
   renderEvents,
@@ -28,21 +29,18 @@ const placeholderEvents = [
 ];
 const placeholderSponsorListings = [
   {
-    title: "HACS x Cisco",
-    date: "3/3/2021",
-    startTime: "7:00pm CST",
+    title: "Cisco Technical Consulting Engineer Co-op",
+    timeline: {
+      openDate: null,
+      closeDate: null,
+    },
     img:
       "http://drive.google.com/uc?export=view&id=16DFXoFE6g_GNrYvBZYYa-hmBxVU53Z8C",
-    meetingLink:
-      "https://cisco.webex.com/cisco/j.php?MTID=m56c5cf702bc5043269cece567e3efe5b",
-    rsvpLink: "https://cisco.avature.net/su/c82e80f1ecff7432",
-    location:
-      "https://cisco.webex.com/cisco/j.php?MTID=m56c5cf702bc5043269cece567e3efe5b",
+    link: "http://bit.ly/tcecoop21",
     description:
-      "During this networking night, students will really get to know Cisco, who they are and what they do. Alongside a panel of some of their best LatinX leaders, students will be broken into breakout rooms with their volunteers to learn about their culture, their LatinX professional network organization (Conexion), the various roles open at Cisco and how they give back to their communities! All through casual networking, no need for formalities, just be yourself. 😊",
+      "Cisco doesn't have many roles open right now, however they are recruiting actively for their technical consulting engineer co-op.",
     otherLinks: {
       flyerLink: "http://bit.ly/37Xh8Aq",
-      jobListing: "http://bit.ly/tcecoop21",
     },
   },
 ];
@@ -50,11 +48,10 @@ const placeholderSponsorScholarships = [
   {
     title: "Texas Computer Science Scholarships for Diversity",
     timeline: {
-      startDate: "12/4/2020",
-      endDate: "3/3/2021",
+      openDate: "12/4/2020",
+      closeDate: "3/3/2021",
     },
-    img:
-      "http://drive.google.com/uc?export=view&id=13vJOh7uXtNfc85pexo4proasGr_k8ajg",
+    img: null,
     link: "https://hornraiser.utexas.edu/project/22570",
     description:
       "As the largest top-10 computer science program in the country, Texas Computer Science is uniquely positioned to tackle head-on the demand for bolstering diversity in tech. The Texas Computer Science Endowment for Change was established to support diversity efforts by funding opportunities for underserved students with a demonstrated passion for computer science. Scholarships through the Texas Computer Science Endowment for Change are available to incoming and current undergraduate computer science students who have participated in The Code Longhorn program or are members of or plan to join the Association of Black Computer Scientists (ABCS), or the Hispanic Association of Computer Scientists (HACS).",
@@ -115,40 +112,71 @@ function Opportunities(props) {
 
   sponsorEvents = placeholderEvents;
   sponsorScholarships = placeholderSponsorScholarships;
+  sponsorListings = placeholderSponsorListings;
 
-  return (
-    <div className="opportunities">
-      <h1>Opportunities</h1>
-      <p>
-        We will post as many amazing opportunities here as we can! These include
-        scholarships, events, programs, and job listings for our membership!
-      </p>
-      <div className="job-listings">
-        <div className="sponsor-listings flex-row">
-          {renderJobListings(sponsorListings)}
-        </div>
-        <div className="other-listings flex-row">
-          {renderJobListings(nonSponsorListings)}
-        </div>
-      </div>
-      <div className="corporate-events">
-        <h3 className="section-title">Events from our sponsors</h3>
-        <div className="sponsor-events flex-row">
-          {renderEvents(sponsorEvents)}
-        </div>
-        <div className="other-events flex-row">
-          {renderEvents(nonSponsorEvents)}
-        </div>
-      </div>
-      <div className="scholarships">
-        <h3 className="section-title">Events from our sponsors</h3>
+  const scholarships = (
+    <div className="scholarships">
+      <Fade bottom>
+        <h3 className="section-title">Scholarships</h3>
+      </Fade>
+      <Fade left>
         <div className="sponsor-scholarships flex-row">
           {renderScholarships(sponsorScholarships)}
         </div>
         <div className="other-scholarships flex-row">
           {renderScholarships(nonSponsorScholarships)}
         </div>
-      </div>
+      </Fade>
+    </div>
+  );
+
+  const events = (
+    <div className="corporate-events">
+      <Fade bottom>
+        <h3 className="section-title">Events</h3>
+      </Fade>
+      <Fade right>
+        <div className="sponsor-events flex-row">
+          {renderEvents(sponsorEvents)}
+        </div>
+        <div className="other-events flex-row">
+          {renderEvents(nonSponsorEvents)}
+        </div>
+      </Fade>
+    </div>
+  );
+
+  const jobListings = (
+    <div className="job-listings">
+      <Fade bottom>
+        <h3 className="section-title">Job Listings</h3>
+      </Fade>
+      <Fade left>
+        <div className="sponsor-listings flex-row">
+          {renderJobListings(sponsorListings)}
+        </div>
+        <div className="other-listings flex-row">
+          {renderJobListings(nonSponsorListings)}
+        </div>
+      </Fade>
+    </div>
+  );
+
+  return (
+    <div className="opportunities">
+      <Fade>
+        <section>
+          <h1>Opportunities</h1>
+          <p>
+            We will post as many amazing opportunities here as we can! These
+            include scholarships, events, programs, and job listings for our
+            membership!
+          </p>
+        </section>
+      </Fade>
+      {scholarships}
+      {events}
+      {jobListings}
     </div>
   );
 }
--- src/components/Opportunities/utils.js
+++ src/components/Opportunities/utils.js
@@ -6,8 +6,57 @@ function renderLinks(links) {
 }
 
 // Return JSX of a timeline
-function renderTimeline(timeline) {
-  return <div className="other-links" />;
+function renderTimeline(t) {
+  return (
+    <div className="timeline-container">
+      <h4 className="event-start-time">
+        {t.openDate ? `Opens: ${t.openDate}` : "NOW OPEN"}
+      </h4>
+      {t.closeDate ? (
+        <h4 className="event-end-time">Closes: {t.closeDate}</h4>
+      ) : null}
+    </div>
+  );
+}
+
+// Return JSX for an array of job-listings
+export function renderJobListings(listings) {
+  console.log(listings);
+
+  return listings.map((l) => (
+    <div className="job-listing-container opportunity flex card">
+      {l.img ? <img src={l.img} className="scholarship-image" /> : null}
+      <div className="details">
+        <h3 className="title">{l.title}</h3>
+        <div className="job-timeline-container">
+          {renderTimeline(l.timeline)}
+        </div>
+        <a href={l.link} className="job-link">
+          <button>Visit Site</button>
+        </a>
+        <div className="other-links-container">{renderLinks(l.otherLinks)}</div>
+      </div>
+    </div>
+  ));
+
+  // return listings.map((l) => (
+  //   <div className="job-listing-container opportunity flex card">
+  //     {l.img ? <img src={l.img} className="scholarship-image" /> : null}
+  //     <div className="details">
+  //       <h3 className="title">{l.title}</h3>
+  //       <p className="job-description">{l.description}</p>
+  //       <div className="job-timeline-container">
+  //         {renderTimeline(l.timeline)}
+  //       </div>
+  //       <a href={l.link} className="job-link">
+  //         <button>Visit Site</button>
+  //       </a>
+  //       <div className="other-links-container">
+  //         {renderLinks(l.otherLinks)}
+  //       </div>
+  //     </div>
+  //   </div>
+  // ));
 }
 
 // Return JSX for an array of events
@@ -16,18 +65,21 @@ export function renderEvents(events) {
 
   return events.map((e) => (
     <div className="event-container opportunity flex card" key={e.slug}>
-      <h3 className="title">{e.title}</h3>
-      <div className="event-time-container">
-        <h4 className="event-start-time">{e.startTime}</h4>
-        <h4 className="event-end-time">{e.endTime}</h4>
-      </div>
-      <div className="event-links-container flex-row">
-        <a href={e.rsvpLink} className="rsvp-link">
-          <button>RSVP</button>
-        </a>
-        <a href={e.meetingLink} className="meeting-link">
-          <button>JOIN</button>
-        </a>
+      {e.img ? <img src={e.img} className="event-image" /> : null}
+      <div className="details">
+        <h3 className="title">{e.title}</h3>
+        <div className="event-time-container">
+          <h4 className="event-start-time">{e.startTime}</h4>
+          <h4 className="event-end-time">{e.endTime}</h4>
+        </div>
+        <div className="event-links-container flex-row">
+          <a href={e.rsvpLink} className="rsvp-link">
+            <button>RSVP</button>
+          </a>
+          <a href={e.meetingLink} className="meeting-link">
+            <button>JOIN</button>
+          </a>
+        </div>
       </div>
     </div>
   ));
@@ -54,36 +106,21 @@ export function renderEvents(events) {
   // ));
 }
 
-// Return JSX for an array of job-listings
-export function renderJobListings(listings) {
-  console.log(listings);
-
-  return listings.map((l) => (
-    <div className="job-listing-container opportunity flex card">
-      <img src={l.img} className="scholarship-image" />
-      <h1 className="title">{l.title}</h1>
-      <a href={l.link} className="job-link">
-          <button>Visit Site</button>
-      </a>
-      <div className="job-timeline-container">{renderTimeline(l.timeline)}</div>
-      <p className="job-description">{l.description}</p>
-      <div className="other-links-container">{renderLinks(l.otherLinks)}</div>
-    </div>
-  ));
-}
-
 // Return JSX for an array of scholarships
 export function renderScholarships(scholarships) {
   console.log(scholarships);
 
   return scholarships.map((s) => (
     <div className="scholarship-container opportunity flex card">
-      <h3 className="title">{s.title}</h3>
-      <a href={s.link} className="scholarship-link">
-        <button>Visit Site</button>
-      </a>
-      <div className="scholarship-timeline-container">
-        {renderTimeline(s.timeline)}
+      {s.img ? <img src={s.img} className="scholarship-image" /> : null}
+      <div className="details">
+        <h3 className="title">{s.title}</h3>
+        <a href={s.link} className="scholarship-link">
+          <button>Visit Site</button>
+        </a>
+        <div className="scholarship-timeline-container">
+          {renderTimeline(s.timeline)}
+        </div>
       </div>
     </div>
   ));
@@ -91,7 +128,6 @@ export function renderScholarships(scholarships) {
   // return scholarships.map((s) => (
   //   <div className="scholarship-container flex card">
   //     <img src={s.img} className="scholarship-image" />
-
   //     <h1 className="scholarship-title">{s.title}</h1>
   //     <a href={s.link} className="scholarship-link">
   //       <button>Visit Site</button>
